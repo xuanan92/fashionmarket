7536fbe92a2453923a5525e654bdca9d0ac56ff9
xuanan92 <48396734+xuanan92@users.noreply.github.com>
2023-02-11 18:55:23 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ReactDOM from "react-dom/client"
 import App from './App'
 import './index.css'
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
 root.render(
   <>
